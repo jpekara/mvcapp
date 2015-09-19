dfd9aebf4dc70e90a331fa413c8fc5250c06e1de
jpekara <pekara.j@gmail.com>
2015-09-19 10:41:47 +0200

    Create User.cs

---
+++ User.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace zadanie_insert.Models
+{
+    public class User
+    {
+        public int id { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+    }
+}
