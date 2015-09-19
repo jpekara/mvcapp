62efd0c1dc454bd97b23a7a20c00860ab0ba51dc
jpekara <pekara.j@gmail.com>
2015-09-19 10:48:42 +0200

    Create SQLQuery2.sql

---
+++ SQLQuery2.sql
@@ -0,0 +1,29 @@
+USE [TestDb]
+GO
+
+/****** Object:  Table [dbo].[UserTable]    Script Date: 19.9.2015 10:45:12 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+SET ANSI_PADDING ON
+GO
+
+CREATE TABLE [dbo].[UserTables](
+	[Id] [int] NOT NULL,
+	[FirstName] [varchar](50) NOT NULL,
+	[LastName] [varchar](50) NOT NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+
+SET ANSI_PADDING OFF
+GO
+
+
